a7f03e7309f26344c92e6f17754b118172c39841
Paramasivam Sajeepan <sajsajee18@gmail.com>
2023-10-17 15:14:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,186 @@
+
+
+// day2 JavaScript exercise
+
+// start
+// 1,2
+// var j = "Learn JavaScript";
+// console.log(j);
+
+
+// //3
+// console.log(j.length);
+
+
+// //4
+// console.log(j.toUpperCase());
+
+
+// //5
+// console.log(j.toLowerCase());
+
+
+// //6
+// console.log(j.substr(-1));
+// console.log(j.substring(15));
+// // end with same variable j
+
+
+// //7
+// let learn = "Days Of JavaScript from learn Javascript";
+// console.log(learn.length);
+// console.log(learn.substring(19));
+
+
+// //8
+// console.log(learn.includes("hi"))  /* identify the letters in that variable */
+
+
+// //9
+// var split= 'learn JavaScript';
+// var split1 = split.split("");
+// console.log(split1);
+
+
+// //10
+// var split2= 'learn JavaScript';
+// var split3 = split2.split(" ");
+// console.log(split3);
+
+
+// //11
+// let social = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+// let media = social.split(',');
+// console.log(media);
+
+
+// //12
+// let replaced = "learn Javascript";
+// console.log(replaced.replace("learn Javascript","learn Javascript to 30 Days Of Python "));
+
+// //13
+// let  character = 'learn Javascript'
+// console.log(character.length);
+// console.log( character.charAt(15)) ;
+
+
+// //14
+// let character1 = 'learn Javascript'
+// console.log(character1.charCodeAt(3)) //value of ASCII
+
+
+// //15
+// var position = 'learn Javascript';
+// console.log(position.indexOf('a'));
+
+// //16
+// var position1 = 'learn Javascript';
+// console.log(position1.lastIndexOf('a'));
+
+// //17
+// var position2 = "You cannot end a sentence with because because because is a conjunction";
+// console.log(position2.indexOf("because"));
+
+// //18
+// var position3 = "You cannot end a sentence with because because because is a conjunction";
+// console.log(position3.lastIndexOf("because"));
+
+// //19
+// var searchh = "You cannot end a sentence with because because because is a conjunction";
+// console.log(searchh.search("because"));
+
+// //20
+// var removespace = " You cannot end a sentence with because because because is a conjunction ";
+// console.log(removespace);
+// console.log(removespace.trim(" "));
+
+// //21
+// var start = 'learn Javascript';
+// console.log(start.startsWith("lea"));
+// console.log(start.startsWith("java"));
+
+// //22
+// var end = 'learn Javascript';
+// console.log(end.endsWith("lea"));
+// console.log(end.endsWith("Javascript"));
+
+// //23
+// let matchh = "learn javascript"
+// console.log(matchh.match("a"));
+
+// //24
+// let concats = 'learn javascript'
+// console.log(concats.concat(" 30 Days Of", " JavaScript"));
+
+// //25
+// let repeats= 'learn javascript'
+// console.log(repeats.repeat(2));
+
+
+// array wrong
+// var a=new Array("ball","bat");
+// console.log(new Array[a]);
+
+// var z=[[1,2,3],["a","b","c"]];
+// console.log(z);
+// console.log(z[1][2]);
+
+// var w=[]
+// w[100]= 1
+// console.log(w)
+// console.log(w[100])
+
+ // object inside the Array
+// home work
+
+// tostring
+// var Number= [1,2,3,4];
+// var Number1= Number.toString();
+// console.log(typeof Number1)
+
+//join
+// var a=["a","b","c","d"];
+// var a1=a.join("+");
+// console.log(a1);
+
+
+//pop( hide last word)
+// var a=["a","b","c","d"];
+// a.pop();
+// console.log(a);
+
+//push ( add from the end)
+// var a=["a","b","c","d"];
+// a.push(1,null,"j");
+// console.log(a);
+// var b= a.push(1,null,"j");
+// console.log(b)
+
+
+//shift (remove from beginning)
+//unshift (add from beginning)
+
+//length (measure from one not 0)
+// const num=[1,2,3,4]
+// console.log(num.length);
+
+// sort
+
+// reverse
+
+// delete( length will be there(space))
+
+// slice2 ( after 2 will be sliced or deleted )no changes in originsl element
+// names=["a","b","c","d"];
+// let wow =names.slice(2,4)
+// console.log(names)
+// console.log(wow)
+
+// (-2)
+
+// splice(can make changes in original element)
+
+
+
+
+
